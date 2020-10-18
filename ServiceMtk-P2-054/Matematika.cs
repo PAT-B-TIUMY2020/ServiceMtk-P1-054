4a63056186421f3903dc2bfc5ac7e3822fb3c12c
Muhammad Ferdy Yosa <55939744+FerdyYosa@users.noreply.github.com>
2020-10-18 22:46:18 +0700

    First Commit

---
+++ ServiceMtk-P2-054/Matematika.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P2_054
+{
+    public class Matematika : IMatematika
+    {
+        public int Bagi(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Kali(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Kurang(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+
+        public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        {
+            Koordinat hasil = new Koordinat();
+            hasil.X = a.X + b.X;
+            hasil.Y = a.Y + b.Y;
+            return hasil;
+        }
+    }
+}
